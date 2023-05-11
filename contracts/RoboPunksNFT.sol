ed7bba736016221db143268d7462696646ef68e7
farzad sanjarani <farzadsanjarani3@gmail.com>
2023-05-11 19:03:52 +1000

    co2

--- contracts/RoboPunksNFT.sol
+++ contracts/RoboPunksNFT.sol
@@ -1,5 +1,28 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.18;
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import '@openzeppelin/contracts/access/Ownable.sol';
+pragma solidity ^0.8.4;
 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract RoboPunksNFT is ERC721, Ownable {
+    uint256 public mintPrice;
+    uint256 public totalSupply;
+    uint256 public maxSupply;
+    uint256 public maxPerWallet;
+    bool public isPublicMintEnabled;
+    string internal baseTokenUrl;
+    address payable public withdrawWallet;
+    mapping(address => uint256) public walletMints;
+
+    constructor() payable ERC721("RoboPunks", "RP") {
+        mintPrice = 0.2 ether;
+        totalSupply = 0;
+        maxSupply = 1000;
+        maxPerWallet = 3;
+        //set withdraw wallet address
+    }
+
+    function setIsPublicMintEnabled(bool isPublicMintEnabled_) external onlyOwner{
+        isPublicMintEnabled = isPublicMintEnabled_;
+    }
+}
