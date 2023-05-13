0d6e765b9b11c64def4c0689bd866808da70de38
farzad sanjarani <farzadsanjarani3@gmail.com>
2023-05-13 14:36:49 +1000

    co3

--- contracts/RoboPunksNFT.sol
+++ contracts/RoboPunksNFT.sol
@@ -25,4 +25,31 @@ contract RoboPunksNFT is ERC721, Ownable {
     function setIsPublicMintEnabled(bool isPublicMintEnabled_) external onlyOwner{
         isPublicMintEnabled = isPublicMintEnabled_;
     }
+
+    function setBaseTokenUri(string calldata baseTokenUri_) external onlyOwner{
+        baseTokenUri = baseTokenUri_;
+    }
+
+    function tokenURL(uint256 tokenId_) public view override returns (string memory){
+        require(_exists(tokenId_), 'Token does not exist!');
+        return string(abi.encodePacked(baseTokenUri, Strings.toString(tokenId_), '.json'));
+    }
+
+    function withdraw() external onlyOwner{
+        (bool success, ) = withdrawWallet.call{ value: address(this).balance}('');
+        require(success, 'withdraw failed');
+    }
+
+    function mint(uint256 quantity_) public payable{
+        require(isPublicMintEnabled, 'minting not enabled');
+        require(msg.value == quantity_ * mintPrice, ' wrong mint value');
+        require(totalSupply + quantity_ <= maxSupply, 'sold out');
+        require(walletMints[msg.sender] + quantity_ <=maxPerWallet, 'exceed max wallet');
+
+        for(uint256 i=0; i<quantity_;i++){
+            uint256 newTokenId = totalSupply + 1;
+            totalSupply++;
+            _safeMint(msg.sender, newTokenId);
+        }
+    }
 }
